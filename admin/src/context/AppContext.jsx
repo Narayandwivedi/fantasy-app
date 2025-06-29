7544b95fdd68510a6c4863261c6f69f9794c0b68
Narayandwivedi <desinplus1@gmail.com>
2025-06-29 23:11:59 +0530

    c

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -6,8 +6,8 @@ import axios from "axios";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  // const BACKEND_URL = "https://fantasy-i3uytsgrm-narayan-dwivedis-projects.vercel.app";
-  const BACKEND_URL = 'http://localhost:4000'
+  const BACKEND_URL = "https://fantasy-i3uytsgrm-narayan-dwivedis-projects.vercel.app";
+  // const BACKEND_URL = 'http://localhost:4000'
   const [allPlayers, setAllPlayers] = useState([]);
 
   const fetchAllPlayers = async () => {
